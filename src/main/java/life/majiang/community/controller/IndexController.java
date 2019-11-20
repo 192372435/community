7080b2a6f1b058eb1042c9bdc025459f2e9cdc80
192372435 <192372435@qq.com>
2019-11-20 22:03:07 +0800

    update mysql

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,13 +1,36 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.Mapper.UserMapper;
+import life.majiang.community.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findbytoken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+
+                break;
+            }
+            
+        }
+
         return "index";
     }
 }
